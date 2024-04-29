1d2522336c3f5535fcc94ab5ca4a873af0cc1530
maxmulla23 <maxmulla23@gmail.com>
2024-04-29 19:07:47 +0300

    login form

--- journal/app/components/forms/loginForm.js
+++ journal/app/components/forms/loginForm.js
@@ -36,7 +36,7 @@ export default function LoginForm(){
       <div className="w-full max-w-xs">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+            <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
               Email
             </label>
               <input 

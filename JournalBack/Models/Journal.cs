a29734266ecfbee274e49bdd5f76a2d04cbb5e71
maxmulla23 <maxmulla23@gmail.com>
2024-06-27 16:21:53 +0300

    journal

--- JournalBack/Models/Journal.cs
+++ JournalBack/Models/Journal.cs
@@ -4,16 +4,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JournalBack.Models
 {
+    [Table("Journals")]
     public class Journal 
     {
         [Key]
         public int Id { get; set;}
-        public string? Title { get; set;}
-        public string? Content { get; set;}
+        public string Title { get; set;} = string.Empty;
+        public string Content { get; set;} = string.Empty;
         public DateTime Date {get; set; } = DateTime.Now;
-        [ForeignKey("User")]
-        [MaxLength(450)]
-        public string UserId { get; set; }
-        public User User { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }

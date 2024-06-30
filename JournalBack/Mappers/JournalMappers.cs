26731bd9a8429bc767484cba6c0a1cf87158a01a
maxmulla23 <maxmulla23@gmail.com>
2024-06-30 15:53:37 +0300

    to create journal

--- JournalBack/Mappers/JournalMappers.cs
+++ JournalBack/Mappers/JournalMappers.cs
@@ -2,11 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JournalBack.Dtos.Journal;
+using JournalBack.Models;
 
 namespace JournalBack.Mappers
 {
     public static class JournalMappers
     {
-        
+        public static JournalDto ToJournalDto(this Journal journal)
+        {
+            return new JournalDto
+            {
+                Id = journal.Id,
+                Title = journal.Title,
+                Content = journal.Content,
+                Date = journal.Date,
+            };
+        }
+
+        public static Journal ToJournalFromCreate(this CreateJournalDto journalDto)
+        {
+            return new Journal
+            {
+                Title = journalDto.Title,
+                Content = journalDto.Content
+            };
+        }
     }
 }
